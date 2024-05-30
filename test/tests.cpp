824d490ee54a2f64a65d1a9e242c2271185d9854
KirFedotov <k.fedotov.guit29@mail.ru>
2024-05-30 19:40:45 +0300

    fix tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -152,4 +152,26 @@ TEST(CircleTest, ComplexTest5) {
     EXPECT_DOUBLE_EQ(circle.getFerence(), 2 * M_PI * 2);
 }
 
+TEST(TasksTest, EarthAndRopeTest) {
+    double rope_len = 10.0;
+    double expected_increase = rope_len / (2 * M_PI);
+    double actual_increase = earthAndRope(rope_len);
+    EXPECT_NEAR(actual_increase, expected_increase, 1e-6);
+}
 
+TEST(TasksTest, PoolCostTest) {
+    double pool_rad = 5.0;
+    double road_wid = 2.0;
+    double concrete_cost = 10.0;
+    double fence_cost = 5.0;
+
+    double concreteArea = M_PI * (std::pow(pool_rad + road_wid, 2)
+     - std::pow(pool_rad, 2));
+    double fenceFerence = 2 * M_PI * (pool_rad + road_wid);
+
+    double expected_cost = concreteArea * concrete_cost
+     + fenceFerence * fence_cost;
+    double actual_cost = poolCost(pool_rad, road_wid,
+     concrete_cost, fence_cost);
+    EXPECT_NEAR(actual_cost, expected_cost, 1e-6);
+}
